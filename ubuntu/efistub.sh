d02d38891b2e21a5d3f743bd7c6f06ecfbf77fe6
Omkar Chandorkar <forumomkar@gmail.com>
2020-01-20 22:24:12 +0530

    Remove the actual command to add efistub entry (for ubuntu) I will soon add a another script for efistub and it will work globally regardless of the 4 distros I support Added documentation (haha yes)

--- ubuntu/efistub.sh
+++ ubuntu/efistub.sh
@@ -1,12 +1,13 @@
+#
+#Add kernel and initramfs to your boot partition
+#
 sudo touch /etc/kernel/postinst.d/zz-update-efistub
 echo '#!/bin/sh' | sudo tee -a /etc/kernel/postinst.d/zz-update-efistub
 echo 'cp /boot/vmlinuz /boot/initrd.img /boot/efi/EFI/ubuntu/' | sudo tee -a /etc/kernel/postinst.d/zz-update-efistub
 sudo chmod +x /etc/kernel/postinst.d/zz-update-efistub
 sudo /etc/kernel/postinst.d/zz-update-efistub
+#
+#Remove grub and all it's dependencies
+#
 sudo apt autoremove --purge -y grub-common grub-pc
 sudo apt install -y efibootmgr
-
-#
-# Make a efistub entry using the following command
-#
-sudo efibootmgr --create --disk "$DISK" --part "$NUM" --loader "$LOADER" --label "$LABEL" --unicode "root=UUID=$UUID rw initrd=$INIT" --verbose
