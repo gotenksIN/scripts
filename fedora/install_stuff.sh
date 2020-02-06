31b3a26cd145ae6d6248f54063ac1d7813bdfbc0
Omkar Chandorkar <forumomkar@gmail.com>
2020-02-06 11:27:28 +0530

    add stuff in arch script and begin work for fedora

---
+++ fedora/install_stuff.sh
@@ -0,0 +1,3 @@
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf install telegram-desktop zsh nano git figlet fortune-mod zsh htop git
