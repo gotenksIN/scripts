9d465bf2fad53e85ef2524a41f628122c5d896dd
Omkar Chandorkar <forumomkar@gmail.com>
2020-06-30 07:31:43 +0530

    ubuntu: update efistub
    now this should actually work

--- ubuntu/efistub.sh
+++ ubuntu/efistub.sh
@@ -1,13 +1,30 @@
-#
+#!/bin/bash
+
 #Add kernel and initramfs to your boot partition
-#
+
 sudo touch /etc/kernel/postinst.d/zz-update-efistub
 echo '#!/bin/sh' | sudo tee -a /etc/kernel/postinst.d/zz-update-efistub
 echo 'cp /boot/vmlinuz /boot/initrd.img /boot/efi/EFI/ubuntu/' | sudo tee -a /etc/kernel/postinst.d/zz-update-efistub
 sudo chmod +x /etc/kernel/postinst.d/zz-update-efistub
 sudo /etc/kernel/postinst.d/zz-update-efistub
-#
+
 #Remove grub and all it's dependencies
-#
+
 sudo apt autoremove --purge -y grub-common grub-pc
 sudo apt install -y efibootmgr
+
+label='Ubuntu (efistub)'
+loader='\EFI\ubuntu\vmlinuz'
+initrd='\EFI\ubuntu\initrd.img'
+
+echo "Enter your boot disk device (e.g. /dev/sda)"
+read disk
+
+echo "Enter partition number for your boot partition"
+read part
+
+printf -v largs "%s " \
+        "root=UUID=$(findmnt -kno UUID /) rw" \
+        "initrd=${initrd}"
+
+ efibootmgr -c -d "${disk}" -p "${part}" -L "${label}" -l "${loader}" -u "${largs%* }" --verbose
