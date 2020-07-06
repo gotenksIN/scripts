6447ec1fa05fcb5fae3320e6175c9db3cc1a4284
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-06 10:01:57 +0530

    nix: update GroundBox config
    changed too much stuff to really mention individually

--- nixos/groundbox.nix
+++ nixos/groundbox.nix
@@ -10,54 +10,93 @@
       ./hardware-configuration.nix
     ];
 
+  nixpkgs.config = {
+    allowUnfree = true;
+    };
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  
+  # Use the latest testing kernel
+    boot.kernelPackages = pkgs.unstable.linuxPackages_testing;
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "GroundBox"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable NetworkManager
+    networking.networkmanager.enable = true;
+  
+  # Disable service for faster boot times
+    systemd.services.NetworkManager-wait-online.enable = false;
+    networking.dhcpcd.enable = false;
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.eno1.useDHCP = true;
-  networking.interfaces.enp0s20f0u1.useDHCP = true;
-  networking.interfaces.wlo1.useDHCP = true;
+  # networking.useDHCP = false;
+  # networking.interfaces.eno1.useDHCP = false;
+  # networking.interfaces.enp0s20f0u1.useDHCP = false;
+  # networking.interfaces.wlo1.useDHCP = false;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
+    i18n.defaultLocale = "en_GB.UTF-8";
+    console = {
+        font = "Lat2-Terminus16";
+        keyMap = "us";
+    };
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Asia/Kolkata";
+
+  # Configure fonts
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs.latest; [
+      cascadia-code
+      pkgs.custom.jetbrains-mono-nerdfonts
+      noto-fonts
+      roboto
+      ubuntu_font_family
+    ];
 
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   wget vim
-  # ];
+    environment.systemPackages = with pkgs; [
+    busybox
+    cmake
+    curl
+    htop
+    lsb-release
+    nano
+    networkmanager
+    ninja
+    plata-theme
+    python38
+    python38Packages.pip
+    python38Packages.python-fontconfig
+    traceroute
+    wget
+    unzip
+    ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
+    programs.gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
   #   pinentryFlavor = "gnome3";
-  # };
+    };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+    services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -69,26 +108,62 @@
   # services.printing.enable = true;
 
   # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
+   services.xserver = {
+        enable = true;
+        displayManager.sddm.enable = true;
+        desktopManager.plasma5.enable = true;
+        libinput.enable = true;
+        layout = "us";
+        };
 
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
+  # Install udev packages
+  services.udev.packages = with pkgs; [
+    android-udev-rules
+    libu2f-host
+    gnome3.gnome-settings-daemon
+  ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.jane = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  # };
+    users.users.gotenks = {
+        isNormalUser = true;
+        shell = pkgs.zsh;
+        extraGroups = [ "wheel" "networkmanager"]; # Enable ‘sudo’ and allow use of 'networkmanager' for the user.
+    };
+
+  # Make sure ~/bin is in $PATH.
+  environment.homeBinInPath = true;
+
+  # Install some packages I use quite often
+    users.users.gotenks.packages = with pkgs; [
+      android-udev-rules
+      aria2
+      bat
+      cmatrix
+      curl
+      figlet
+      fontconfig
+      fortune
+      git
+      google-chrome
+      gnumake
+      htop
+      kotatogram-desktop
+      ncdu
+      neofetch
+      networkmanager
+      noto-fonts
+      noto-fonts-emoji
+      open-sans
+      papirus-icon-theme
+      pfetch
+      tdesktop
+      vlc
+      vscode
+    ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -97,6 +172,8 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.09"; # Did you read the comment?
+
+system.copySystemConfiguration = true;
 
 }
 
