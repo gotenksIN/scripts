606eb426ad56c33eb83fd1066f82b309d2660b38
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-08 09:48:27 +0530

    nixos: enable gnome3 keyring services

--- nixos/groundbox.nix
+++ nixos/groundbox.nix
@@ -104,6 +104,9 @@
   # Enable the OpenSSH daemon.
     services.openssh.enable = true;
 
+  # Enable Gnome3 keyring
+    services.gnome3.gnome-keyring.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
