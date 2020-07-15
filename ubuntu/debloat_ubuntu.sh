f4d1f8d0288d35d06f467d2a2314cb974d262908
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-15 09:03:11 +0530

    ubuntu: remove vim

--- ubuntu/debloat_ubuntu.sh
+++ ubuntu/debloat_ubuntu.sh
@@ -1,5 +1,5 @@
 sudo rm -rf /var/cache/snapd/
-sudo apt autoremove --purge snapd gnome-software-plugin-snap apparmor avahi-daemon rsyslog apport
+sudo apt autoremove --purge snapd gnome-software-plugin-snap apparmor avahi-daemon rsyslog apport ^vim
 rm -fr ~/snap
 sudo systemctl disable NetworkManager-wait-online.service
 sudo systemctl disable apt-daily-upgrade.service
