3e155e75490c9d59a0bf7dc044a8292184354e9f
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-17 20:31:19 +0100

    ubuntu: debloat: remove a bunch of useless fonts/packages

--- ubuntu/debloat_ubuntu.sh
+++ ubuntu/debloat_ubuntu.sh
@@ -1,5 +1,6 @@
 sudo rm -rf /var/cache/snapd/
-sudo apt autoremove --purge snapd gnome-software-plugin-snap apparmor avahi-daemon rsyslog apport ^vim
+sudo apt autoremove --purge snapd gnome-software-plugin-snap apparmor ^avahi rsyslog apport ^vim mobile-broadband-provider-info popularity-contest \
+fonts-beng fonts-deva fonts-gujr fonts-indic fonts-knda fonts-mlym fonts-orya fonts-smc fonts-taml fonts-telu fonts-tibetan-machine fonts-thai-tlwg
 rm -fr ~/snap
 sudo systemctl disable NetworkManager-wait-online.service
 sudo systemctl disable apt-daily-upgrade.service
