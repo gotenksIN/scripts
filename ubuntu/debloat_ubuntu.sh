91bcba693c9cd09b07d5ca4bd6481d0e371cf330
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-17 20:50:37 +0100

    ubuntu: debloat: remove bolt as well

--- ubuntu/debloat_ubuntu.sh
+++ ubuntu/debloat_ubuntu.sh
@@ -2,7 +2,7 @@
 
 sudo rm -rf /var/cache/snapd/
 
-sudo apt autoremove --purge snapd gnome-software-plugin-snap apparmor ^avahi rsyslog apport ^vim mobile-broadband-provider-info popularity-contest \
+sudo apt autoremove --purge snapd gnome-software-plugin-snap apparmor ^avahi rsyslog apport ^vim mobile-broadband-provider-info popularity-contest bolt \
 fonts-beng fonts-deva fonts-gujr fonts-indic fonts-knda fonts-mlym fonts-orya fonts-smc fonts-taml fonts-telu fonts-tibetan-machine fonts-thai-tlwg
 
 rm -rf ~/snap
