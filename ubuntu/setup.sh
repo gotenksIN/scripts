09a7bf8a6ee4637ee591192841c4f56d91e9dcf7
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-17 20:40:11 +0100

    ubuntu: setup: setup my gitconfig

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,1 +1,9 @@
 sudo apt install zsh zsh-autosuggestions fortune-mod figlet git htop cmatrix neofetch aria2 curl
+
+git config --global user.name "Omkar Chandorkar"
+git config --global user.email forumomkar@gmail.com
+git config --global user.signingkey 6D8DEF354ED78DE805938A9D95A33FD984777F70
+git config --global gpg.program gpg
+git config --global commit.gpgsign true
+git config --global pull.rebase false
+git config --global core.editor nano
