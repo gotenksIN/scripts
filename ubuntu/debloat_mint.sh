af66e497cefc3b52af025c4a62b81aa113e8547f
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-18 08:45:07 +0530

    ubuntu: debloat add a shit ton more packages
    removed debloat_mint and unified

--- ubuntu/debloat_mint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo apt remove --purge pix hexchat thunderbird transmission libreoffice gnome-orca gnote graphviz hexchat imagemagick xed xreader xviewer celluloid rhythmbox redshift
