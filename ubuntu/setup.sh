8018b217ccb6385b0ed7f90d97bc68461e5cba3e
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-28 08:44:31 +0530

    ubuntu: setup: hardcode-ish the directory for p10k setup
    since I switched to unattended setup, I wont be ending up in zsh after the installation is done, causing it to not find the custom theme dir

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -4,7 +4,7 @@ sudo apt install zsh zsh-autosuggestions fortune-mod figlet git htop cmatrix neo
                  python3-pip python3-venv ffmpeg vlc
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
 
 git config --global user.name "Omkar Chandorkar"
 git config --global user.email forumomkar@gmail.com
