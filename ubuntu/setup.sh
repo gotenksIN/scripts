9b824bccef85425e8227a70f1208018faefd27df
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-28 08:33:23 +0530

    ubuntu: setup: install ncdu

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install zsh zsh-autosuggestions fortune-mod figlet git htop cmatrix neofetch aria2 curl
+sudo apt install zsh zsh-autosuggestions fortune-mod figlet git htop cmatrix neofetch aria2 curl ncdu
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
