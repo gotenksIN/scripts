b5cb890401a90a98415f7fc5fe140e9a1e38f0f2
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-31 13:08:52 +0530

    ubuntu: setup: install zip and unzip

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install zsh zsh-autosuggestions fortune-mod figlet git htop cmatrix neofetch aria2 curl ncdu \
-                 python3-pip python3-venv
+                 python3-pip python3-venv zip unzip
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
