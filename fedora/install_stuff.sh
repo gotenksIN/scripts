135367d497534ce3a8761271ecc18b7e4daa11ce
Omkar Chandorkar <forumomkar@gmail.com>
2020-08-10 20:01:21 +0530

    fedora: nuke
    since its relatively slower than arch/ubuntu, dnf is meh, I don't see a point in keeping/updating it if I never use it

--- fedora/install_stuff.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-sudo dnf install firefox vlc telegram-desktop zsh nano git figlet fortune-mod zsh htop git
