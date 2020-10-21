96810627812687def29801dc86136cd175a67eca
Omkar Chandorkar <gotenksIN@aosip.dev>
2020-10-21 09:27:56 +0530

    arch: setup: edge > google chrome
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- arch/setup.sh
+++ arch/setup.sh
@@ -7,7 +7,7 @@ cd yay
 makepkg -si
 cd
 
-yay -Sy visual-studio-code-insiders google-chrome kotatogram-desktop-bin
+yay -Sy visual-studio-code-insiders edge kotatogram-desktop-bin
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
