68f22b85ae70cd3ba44a5a6683be5e922ea0cf64
Omkar Chandorkar <gotenksIN@aosip.dev>
2020-11-18 09:55:35 +0530

    ubuntu: setup: install a few more packages
    p7zip-full, knockd, progress and pv
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -13,7 +13,7 @@ sudo apt install microsoft-edge-dev code-insiders
 fi
 
 sudo apt install zsh zsh-autosuggestions fortune-mod figlet git htop cmatrix neofetch aria2 curl ncdu \
-                 python3-pip python3-venv zip unzip bat
+                 python3-pip python3-venv zip unzip bat p7zip-full knockd progress pv
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
