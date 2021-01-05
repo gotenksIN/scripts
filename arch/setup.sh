0cb0cb911dfdb398e37615ff0b3ea010e6968d89
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-01-05 09:21:24 +0530

    arch: setup: update aur package names
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- arch/setup.sh
+++ arch/setup.sh
@@ -7,7 +7,7 @@ cd yay
 makepkg -si
 cd
 
-yay -Sy visual-studio-code-insiders edge kotatogram-desktop-bin
+yay -Sy microsoft-edge-dev-bin kotatogram-desktop-bin visual-studio-code-insiders-bin
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
