294adf660be1e2cd30fc1a31e4d027aa3b4fddca
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-01-05 09:24:14 +0530

    arch: setup: set zsh as default shell
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- arch/setup.sh
+++ arch/setup.sh
@@ -17,3 +17,4 @@ fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
+chsh -s /usr/bin/zsh
