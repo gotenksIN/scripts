4cf8472176712c4bd2ee6dee3da7d79519ba5a8c
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-01-05 09:23:46 +0530

    arch: setup: guard gui packages
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- arch/setup.sh
+++ arch/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy ffmpeg vlc steam fortune-mod figlet zsh htop git cmatrix ncdu nano
+sudo pacman -Sy ffmpeg fortune-mod figlet zsh htop git cmatrix ncdu nano
 
 git clone https://aur.archlinux.org/yay.git --depth 1
 cd yay
@@ -8,7 +8,12 @@ makepkg -si
 cd ..
 rm -rf yay
 
-yay -Sy microsoft-edge-dev-bin kotatogram-desktop-bin visual-studio-code-insiders-bin
+# Guard gui dependent applications behind this
+echo "Do you intend on using GUI? [y/n]: "
+read -r -n1 input
+if [[ "$input" =~ ^[Yy]$ ]]; then
+yay -Sy microsoft-edge-dev-bin kotatogram-desktop-bin visual-studio-code-insiders-bin vlc steam 
+fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
