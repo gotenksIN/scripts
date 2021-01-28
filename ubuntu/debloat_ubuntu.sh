ee6f253aad13f6ef94ae6f7b07f1cbc80b0ab43e
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-01-28 19:33:17 +0530

    ubuntu: debloat: remove trailing \
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- ubuntu/debloat_ubuntu.sh
+++ ubuntu/debloat_ubuntu.sh
@@ -9,9 +9,9 @@ language-selector-common language-selector-gnome language-pack-de-base language-
 hunspell-de-at-frami hunspell-de-ch-frami hunspell-de-de-frami hunspell-en-au hunspell-en-ca hunspell-en-za hunspell-es hunspell-fr \
 hunspell-fr-classical hunspell-it hunspell-pt-br hunspell-pt-pt hunspell-ru language-pack-fr-base language-pack-gnome-en-base language-pack-pt-base \
 fonts-beng fonts-deva fonts-gujr fonts-indic fonts-knda fonts-mlym fonts-orya fonts-smc fonts-taml fonts-telu fonts-tibetan-machine fonts-thai-tlwg \
-fonts-kacst fonts-kacst-one fonts-khmeros-core fonts-lao fonts-lklug-sinhala fonts-sil-abyssinica fonts-sil-padauk fonts-arphic-ukai fonts-arphic-uming \
+fonts-kacst fonts-kacst-one fonts-khmeros-core fonts-lao fonts-lklug-sinhala fonts-sil-abyssinica fonts-sil-padauk fonts-arphic-ukai fonts-arphic-uming
 
-rm -rf ~/snap
+rm -fr ~/snap
 
 sudo systemctl disable NetworkManager-wait-online.service
 sudo systemctl disable apt-daily-upgrade.service
