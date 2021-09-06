c74e414e8f510f4dea70b2856a09a4592a58cf33
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-09-06 09:11:13 +0530

    ubuntu: setup: re-ordering and fixes for gui packages
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+sudo apt install zsh fortune-mod figlet git htop neofetch aria2 curl ncdu \
+                 python3-pip python3-venv zip unzip bat p7zip-full ripgrep
+
+# Install Ookla Speedtest
 curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+sudo apt install speedtest
 
 # Guard gui dependent applications behind this
 echo "Do you intend on using GUI? [y/N]: "
@@ -11,14 +16,9 @@ sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo rm microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
 sudo apt install microsoft-edge-dev code-insiders
 fi
-
-# Update package list
-sudo apt-get update
-
-sudo apt install zsh fortune-mod figlet git htop neofetch aria2 curl ncdu \
-                 python3-pip python3-venv zip unzip bat p7zip-full ripgrep speedtest
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
