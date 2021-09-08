523c2b923042d3cb4cc5264aaa79b22c6b368abd
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-09-08 17:54:01 +0530

    arch: install_cinnamon: bring inline with 2021
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- arch/install_cinnamon.sh
+++ arch/install_cinnamon.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 
 # Install all packages required for cinnamon to work nicely
-sudo pacman -Sy xorg-server cinnamon gnome-terminal gnome-screenshot lightdm eog nemo-fileroller rhythmbox gedit evince
+sudo pacman -Sy xorg-server cinnamon gnome-terminal gnome-screenshot lightdm lightdm-slick-greeter eog nemo-fileroller gedit evince
 sudo localectl set-locale LANG=en_US.UTF-8
 sudo locale-gen
 
 # Setup lightdm along with slick-greeter plus necessary configs
-git clone https://aur.archlinux.org/lightdm-slick-greeter --depth=1
-cd lightdm-slick-greeter
-makepkg -si
-cd ..
-rm -rf lightdm-slick-greeter
 sudo sed -i "s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-slick-greeter/" /etc/lightdm/lightdm.conf
 
 # Install packages related to bluetooth if needed
 echo "Do you have any bluetooth adaptors installed? [y/n]: "
 read -r -n1 input
 if [[ "$input" =~ ^[Yy]$ ]]; then
-sudo pacman -Sy bluez bluez-libs bluez-utils bluez-tools
+sudo pacman -Sy bluez bluez-utils blueman
 sudo systemctl enable bluetooth.service
 fi
 
