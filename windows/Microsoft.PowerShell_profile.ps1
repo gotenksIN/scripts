411ed6623e81db92d7ddfd8ce16ea82c5d6b0b7d
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-09-13 16:50:02 +0530

    windows: powershell: import terminal-icons
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\powerlevel10k_lean.omp.json | Invoke-Expression
 Set-Alias neofetch neofetch.ps1
+Import-Module -Name Terminal-Icons
 clear
