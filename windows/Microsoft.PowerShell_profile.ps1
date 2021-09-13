acc573b003ccbcf60700f58c999ede0e71f9d4ec
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-09-13 16:53:23 +0530

    windows: powershell: add git aliases as powershell functions
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,13 @@
 oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\powerlevel10k_lean.omp.json | Invoke-Expression
 Set-Alias neofetch neofetch.ps1
 Import-Module -Name Terminal-Icons
+function gita([string]$FilePath) { git add "$FilePath" }
+function gitc { git commit -S --signoff }
+function gitcp([string]$Hash) { git cherry-pick "$Hash" }
+function gitcpc { git cherry-pick --continue }
+function gitf([string]$PathToFetch) { git fetch "$PathToFetch" }
+function gitp { git push }
+function gitfp { git push -f }
+function gitr { git reset }
+function gitrh { git reset --hard }
 clear
