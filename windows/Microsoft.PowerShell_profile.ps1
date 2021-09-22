fa90180a410aa741ee5e325af0e0f07821d6f7bd
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-09-22 10:35:27 +0530

    windows: powershell: sync with current changes
    * change to star theme
    * allow two args with gitc, so I can use amend and no-edit
    * use generic arg names for all functions
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # Init oh-my-posh
-oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\powerlevel10k_lean.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\star.omp.json | Invoke-Expression
 
 # Alias winfetch script to neofetch
 Set-Alias neofetch pwshfetch-test-1.ps1
@@ -7,15 +7,15 @@ Set-Alias neofetch pwshfetch-test-1.ps1
 Import-Module -Name Terminal-Icons
 
 # Set git aliases from commmon/aliases
-function gita([string]$FilePath) { git add "$FilePath" }
-function gitc { git commit -S --signoff }
-function gitcp([string]$Hash) { git cherry-pick "$Hash" }
+function gita([string]$Arg1) { git add "$Arg1" }
+function gitc([string]$Arg1, [string]$Arg2) { git commit -S --signoff "$Arg1" "$Arg2" }
+function gitcp([string]$Arg1) { git cherry-pick "$Arg1" }
 function gitcpc { git cherry-pick --continue }
-function gitf([string]$PathToFetch) { git fetch "$PathToFetch" }
-function gitp([string]$WhereToPush, [string]$FromWhereToPush) { git push "$WhereToPush" "$FromWhereToPush" }
-function gitfp([string]$WhereToPush, [string]$FromWhereToPush) { git push -f "$WhereToPush" "$FromWhereToPush" }
-function gitr([string]$Hash) { git reset "$Hash" }
-function gitrh([string]$Hash) { git reset --hard "$Hash" }
+function gitf([string]$Arg1) { git fetch "$Arg1" }
+function gitp([string]$Arg1, [string]$Arg2) { git push "$Arg1" "$Arg2" }
+function gitfp([string]$Arg1, [string]$Arg2) { git push -f "$Arg1" "$Arg2" }
+function gitr([string]$Arg1) { git reset "$Arg1" }
+function gitrh([string]$Arg1) { git reset --hard "$Arg1" }
 
 # Clear terminal once profile is finished loading
 clear
