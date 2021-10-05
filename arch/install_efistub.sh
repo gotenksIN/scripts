9fd8964d1551598cb8607b084509e792678c441f
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-10-05 06:56:26 +0000

    arch: install_efistub: fixup microcode setup
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- arch/install_efistub.sh
+++ arch/install_efistub.sh
@@ -2,14 +2,14 @@
 
 read -e -p "Enter your boot disk device (e.g. /dev/sda): " -i "/dev/sda" disk
 read -e -p "Enter partition number for your boot partition (e.g. if your /boot is in /dev/sda1, enter 1): " -i "1" part
-read -e -p "Enter loader name (e.g. vmlinuz-linux if using `linux`): " -i "vmlinuz-linux" loader
-read -e -p "Enter initrd name (e.g. initramfs-linux.img if using `linux`): " -i "initramfs-linux.img" initrd
+read -e -p "Enter loader name (e.g. vmlinuz-linux): " -i "vmlinuz-linux" loader
+read -e -p "Enter initrd name (e.g. initramfs-linux.img): " -i "initramfs-linux.img" initrd
 read -e -p "Enter label for this entry (Label is what would show up in UEFI entries): " -i "rendumOS (efistub)" label
 
 read -e -p "Do you have an Intel or AMD CPU? (Y/n): " input
 if [[ "$input" =~ ^[Yy]$ ]]; then
 read -e -p "Enter 1 for Intel and 2 for AMD: " cpu
-if [[ "$input" =~ ^[1]$ ]]; then
+if [[ "$cpu" =~ ^[1]$ ]]; then
 sudo pacman -Sy intel-ucode
 microcode="intel-ucode.img"
 else
