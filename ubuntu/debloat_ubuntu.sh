71482a90474bc0eb9b6a148b5065830353890b80
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-10-06 08:41:45 +0530

    ubuntu: debloat_ubuntu: fix gnome-control-center being removed
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- ubuntu/debloat_ubuntu.sh
+++ ubuntu/debloat_ubuntu.sh
@@ -3,9 +3,9 @@
 sudo apt autoremove --purge snapd gnome-software-plugin-snap apparmor rsyslog apport ^vim mobile-broadband-provider-info ^avahi ^cloud \
 hexchat thunderbird transmission ^libreoffice gnome-orca gnote graphviz ^imagemagick celluloid rhythmbox redshift fonts-arundina netplan.io \
 gnome-2048 gnome-calculator gnome-calendar gnome-chess gnome-mahjongg gnome-software-plugin-snap gnome-sudoku bolt ^libreoffice \
-language-selector-common language-selector-gnome language-pack-de-base language-pack-es-base popularity-contest language-pack-ru-base \
+language-pack-de-base language-pack-es-base popularity-contest language-pack-ru-base \
 hunspell-de-at-frami hunspell-de-ch-frami hunspell-de-de-frami hunspell-en-au hunspell-en-ca hunspell-en-za hunspell-es hunspell-fr \
-hunspell-fr-classical hunspell-it hunspell-pt-br hunspell-pt-pt hunspell-ru language-pack-fr-base language-pack-gnome-en-base language-pack-pt-base \
+hunspell-fr-classical hunspell-it hunspell-pt-br hunspell-pt-pt hunspell-ru language-pack-fr-base language-pack-pt-base \
 fonts-beng fonts-deva fonts-gujr fonts-indic fonts-knda fonts-mlym fonts-orya fonts-smc fonts-taml fonts-telu fonts-tibetan-machine fonts-thai-tlwg \
 fonts-kacst fonts-kacst-one fonts-khmeros-core fonts-lao fonts-lklug-sinhala fonts-sil-abyssinica fonts-sil-padauk fonts-arphic-ukai fonts-arphic-uming
 
