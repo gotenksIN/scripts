af9940c34d8c67ecc16a2d59a4102d8aca782040
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-10-06 16:21:45 +0530

    ubuntu: debloat: remove more gnome crap
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- ubuntu/debloat_ubuntu.sh
+++ ubuntu/debloat_ubuntu.sh
@@ -3,7 +3,7 @@
 sudo apt autoremove --purge snapd gnome-software-plugin-snap apparmor rsyslog apport ^vim mobile-broadband-provider-info ^avahi ^cloud \
 hexchat thunderbird transmission ^libreoffice gnome-orca gnote graphviz ^imagemagick celluloid rhythmbox redshift fonts-arundina netplan.io \
 gnome-2048 gnome-calculator gnome-calendar gnome-chess gnome-mahjongg gnome-software-plugin-snap gnome-sudoku bolt ^libreoffice \
-language-pack-de-base language-pack-es-base popularity-contest language-pack-ru-base \
+language-pack-de-base language-pack-es-base popularity-contest language-pack-ru-base gnome-disk-utility gnome-font-viewer ^gnome-games \
 hunspell-de-at-frami hunspell-de-ch-frami hunspell-de-de-frami hunspell-en-au hunspell-en-ca hunspell-en-za hunspell-es hunspell-fr \
 hunspell-fr-classical hunspell-it hunspell-pt-br hunspell-pt-pt hunspell-ru language-pack-fr-base language-pack-pt-base \
 fonts-beng fonts-deva fonts-gujr fonts-indic fonts-knda fonts-mlym fonts-orya fonts-smc fonts-taml fonts-telu fonts-tibetan-machine fonts-thai-tlwg \
