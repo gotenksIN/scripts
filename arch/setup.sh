c1af33ead9ffc1ffda04a176b9e4e5e29361fd3a
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-10-14 18:34:31 +0530

    arch: setup: update installed packages
    * sorry linux gaming, ya ain't as painless as I want it to be
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- arch/setup.sh
+++ arch/setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 sudo pacman -Sy git base-devel fortune-mod figlet zsh htop ncdu nano bottom \
-                wget2 curl aria2 inetutils bat ripgrep p7zip efibootmgr neofetch
+                wget curl aria2 inetutils bat ripgrep p7zip efibootmgr neofetch \
+                screen ccache
 
 git clone https://aur.archlinux.org/yay.git --depth 1
 cd yay
@@ -14,11 +15,11 @@ yay -Sy ookla-speedtest-bin
 # Guard gui dependent applications behind this
 read -e -p "Do you intend on using GUI? [y/n]: " input
 if [[ "$input" =~ ^[Yy]$ ]]; then
-yay -Sy microsoft-edge-dev-bin kotatogram-desktop-bin vlc steam \
+yay -Sy microsoft-edge-dev-bin kotatogram-desktop-bin vlc \
         visual-studio-code-insiders-bin ttf-dejavu ttf-droid \
         gnu-free-fonts ttf-liberation noto-fonts noto-fonts-cjk \
         noto-fonts-emoji noto-fonts-extra ttf-ubuntu-font-family \
-        gsfonts qbittorrent
+        gsfonts qbittorrent discord spotify
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
