89b6d1482bdf7e87e339e38315505874e812a07d
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-11-13 13:01:01 +0530

    ubuntu: debloat: let's keep netplan
    * else rip internet on gtk-based de, why?
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- ubuntu/debloat_ubuntu.sh
+++ ubuntu/debloat_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt autoremove --purge snapd gnome-software-plugin-snap apparmor rsyslog apport ^vim mobile-broadband-provider-info ^avahi ^cloud \
-hexchat thunderbird transmission ^libreoffice gnome-orca gnote graphviz ^imagemagick celluloid rhythmbox redshift fonts-arundina netplan.io \
+hexchat thunderbird transmission ^libreoffice gnome-orca gnote graphviz ^imagemagick celluloid rhythmbox redshift fonts-arundina \
 gnome-2048 gnome-calculator gnome-calendar gnome-chess gnome-mahjongg gnome-software-plugin-snap gnome-sudoku bolt ^libreoffice \
 language-pack-de-base language-pack-es-base popularity-contest language-pack-ru-base gnome-disk-utility gnome-font-viewer ^gnome-games \
 hunspell-de-at-frami hunspell-de-ch-frami hunspell-de-de-frami hunspell-en-au hunspell-en-ca hunspell-en-za hunspell-es hunspell-fr \
