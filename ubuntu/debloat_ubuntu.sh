95d53d5e94538c4f792f3be3acf47a042e7302c1
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-11-19 10:23:03 +0530

    ubuntu: debloat: remove unattended_upgrades
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- ubuntu/debloat_ubuntu.sh
+++ ubuntu/debloat_ubuntu.sh
@@ -5,7 +5,7 @@ hexchat thunderbird transmission ^libreoffice gnome-orca gnote graphviz ^imagema
 gnome-2048 gnome-calculator gnome-calendar gnome-chess gnome-mahjongg gnome-software-plugin-snap gnome-sudoku bolt ^libreoffice \
 language-pack-de-base language-pack-es-base popularity-contest language-pack-ru-base gnome-disk-utility gnome-font-viewer ^gnome-games \
 hunspell-de-at-frami hunspell-de-ch-frami hunspell-de-de-frami hunspell-en-au hunspell-en-ca hunspell-en-za hunspell-es hunspell-fr \
-hunspell-fr-classical hunspell-it hunspell-pt-br hunspell-pt-pt hunspell-ru language-pack-fr-base language-pack-pt-base \
+hunspell-fr-classical hunspell-it hunspell-pt-br hunspell-pt-pt hunspell-ru language-pack-fr-base language-pack-pt-base unattended-upgrades \
 fonts-beng fonts-deva fonts-gujr fonts-indic fonts-knda fonts-mlym fonts-orya fonts-smc fonts-taml fonts-telu fonts-tibetan-machine fonts-thai-tlwg \
 fonts-kacst fonts-kacst-one fonts-khmeros-core fonts-lao fonts-lklug-sinhala fonts-sil-abyssinica fonts-sil-padauk fonts-arphic-ukai fonts-arphic-uming
 
