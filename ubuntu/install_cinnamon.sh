13b2f3cbb97425338df0c5f84f07007f1eb245ae
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-12-12 12:16:52 +0530

    ubuntu: cinnamon: install alacritty and dconf-editor
    * yay, can finally fix alt + prtscr bug
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- ubuntu/install_cinnamon.sh
+++ ubuntu/install_cinnamon.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-sudo apt install cinnamon lightdm slick-greeter arc-theme papirus-icon-theme
+sudo add-apt-repository ppa:aslatter/ppa
+sudo add-apt-repository ppa:ubuntucinnamonremix/unstable
+
+sudo apt install cinnamon lightdm slick-greeter arc-theme papirus-icon-theme dconf-editor alacritty
