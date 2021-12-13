3858b916a958e904092023446b44396e113992f9
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-12-13 10:24:57 +0530

    arch: remove hacks for gnome-terminal
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- arch/install_cinnamon.sh
+++ arch/install_cinnamon.sh
@@ -2,8 +2,6 @@
 
 # Install all packages required for cinnamon to work nicely
 yay -Sy xorg-server cinnamon gnome-screenshot lightdm lightdm-slick-greeter nemo-fileroller gnome-keyring pipewire pipewire-pulse papirus-icon-theme arc-gtk-theme dconf-editor
-sudo localectl set-locale LANG=en_US.UTF-8
-sudo locale-gen
 
 # Setup lightdm along with slick-greeter plus necessary configs
 sudo sed -i "s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-slick-greeter/" /etc/lightdm/lightdm.conf
