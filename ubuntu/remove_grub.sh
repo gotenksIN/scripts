ccfce906fc8426dc47c3903d15102400d888f2c9
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-04-10 10:49:27 +0530

    ubuntu: remov_grub: remove shim-signed as well
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- ubuntu/remove_grub.sh
+++ ubuntu/remove_grub.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #Remove grub and all it's dependencies
-sudo apt autoremove --purge -y --allow-remove-essential grub-common grub-pc
+sudo apt autoremove --purge -y --allow-remove-essential grub-common grub-pc shim-signed
