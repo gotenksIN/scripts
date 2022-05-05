0a7f0054da263a920bd75839792b19346a5fac2f
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-05-05 18:25:15 +0530

    ubuntu: install_efistub: allow passing extra kernel cmdline parameters
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- ubuntu/install_efistub.sh
+++ ubuntu/install_efistub.sh
@@ -14,9 +14,10 @@ initrd='\EFI\ubuntu\initrd.img'
 read -e -p "Enter your boot disk device (e.g. /dev/sda): " -i "/dev/sda" disk
 read -e -p "Enter partition number for your boot partition (e.g. if your /boot is in /dev/sda1, enter 1): " -i "1" part
 read -e -p "Enter label for this entry (Label is what would show up in UEFI entries): " -i "ubuntu (efistub)" label
+read -e -p "Enter any extra arguments you want to pass to kernel cmdline: " extra
 
 printf -v largs "%s " \
         "root=UUID=$(findmnt -kno UUID /) rw" \
         "initrd=${initrd}"
 
-sudo efibootmgr -c -d "${disk}" -p "${part}" -L "${label}" -l "${loader}" -u "${largs%* } quiet splash" --verbose
+sudo efibootmgr -c -d "${disk}" -p "${part}" -L "${label}" -l "${loader}" -u "${largs%* } quiet splash ${extra}" --verbose
