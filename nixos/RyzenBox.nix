f56301ef98a1fcd8fa6fcdcc0b67dc87d5f8cbf5
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-05-27 11:18:15 +0530

    nixos: use latest kernel
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- nixos/RyzenBox.nix
+++ nixos/RyzenBox.nix
@@ -18,6 +18,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "RyzenBox"; # Define your hostname.
 
   # Set your time zone.
