007a6a2740a6ef086cd5e5f5e4dca033a9b11792
Omkar Chandorkar <gotenksIN@aospa.co>
2022-09-20 12:48:00 +0530

    fedora: add debloat script
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

---
+++ fedora/debloat_fedora.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo dnf5 remove firefox libreoffice gnome-boxes gnome-calculator gnome-calendar gnome-clocks\
+    gnome-contacts gnome-disk-utility gnome-disk-utility gnome-maps gnome-photos gnome-remote-desktop \
+    gnome-system-monitor gnome-text-editor gnome-video-effects gnome-weather gnome-tour lohit-telugu-fonts \
+    lohit-tamil-fonts lohit-odia-fonts lohit-marathi-fonts lohit-kannada-fonts lohit-gujarati-fonts \
+    lohit-devanagari-fonts lohit-bengali-fonts lohit-assamese-fonts ImageMagick
