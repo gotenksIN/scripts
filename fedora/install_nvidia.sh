4991376af27f717f2d2f65712f2e392e18db0258
Omkar Chandorkar <gotenksIN@aospa.co>
2022-09-20 16:38:07 +0530

    fedora: update nvidia modules installer
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- fedora/install_nvidia.sh
+++ fedora/install_nvidia.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda
-sudo dnf update -y
-
-echo "Do you have Intel integrated GPU and discrete NVIDIA GPU? (Y/n)"
-read inputs
-if [[ $inputs == "Y" || $inputs == "y" ]]; then
-sudo -s
-dnf update
-cat > /etc/modprobe.d/nvidia.conf <<EOF
-options nvidia NVreg_DynamicPowerManagement=0x02
-EOF
-fi
+# Install nvidia kernel modules
+sudo dnf install akmod-nvidia
