a7d8d594af7776b0c7fee38a83611793c1e18710
Omkar Chandorkar <gotenksIN@aospa.co>
2022-10-09 12:35:36 +0530

    common: one script to rule them all
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- common/setup.sh
+++ common/setup.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-cp -r ~/scripts/common/\.* ~/
+if [[ "$(command -v apt)" != "" ]]; then
+    echo "Debian/Ubuntu Based Distro Detected"
+    bash ~/scripts/ubuntu/setup.sh
+    cp -r ~/scripts/common/\.* ~/
+    echo "source ~/scripts/ubuntu/alias" >> ~/.zshrc
+elif [[ "$(command -v pacman)" != "" ]]; then
+    echo "Arch Based Distro Detected"
+    bash ~/scripts/arch/setup.sh
+    cp -r ~/scripts/common/\.* ~/
+    echo "source ~/scripts/arch/alias" >> ~/.zshrc
+elif [[ "$(command -v dnf)" != "" ]]; then
+    echo "Fedora Based Distro Detected"
+    bash ~/scripts/fedora/setup.sh
+    cp -r ~/scripts/common/\.* ~/
+    echo "source ~/scripts/fedora/alias" >> ~/.zshrc
+fi
