79f16ec4a6e3a258a4b9224b01240ec8a3ddf903
Yash-Garg <yash.garg@outlook.in>
2022-11-03 17:42:03 +0530

    fix: use `@args` keyword provided by powershell

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -7,15 +7,15 @@ Set-Alias neofetch pwshfetch-test-1.ps1
 Import-Module -Name Terminal-Icons
 
 # Set git aliases from commmon/aliases
-function gita([string]$Arg1) { git add "$Arg1" }
-function gitc([string]$Arg1, [string]$Arg2) { git commit -S --signoff "$Arg1" "$Arg2" }
-function gitcp([string]$Arg1) { git cherry-pick "$Arg1" }
+function gita { git add @args }
+function gitc { git commit -S --signoff @args }
+function gitcp { git cherry-pick @args }
 function gitcpc { git cherry-pick --continue }
-function gitf([string]$Arg1) { git fetch "$Arg1" }
-function gitp([string]$Arg1, [string]$Arg2) { git push "$Arg1" "$Arg2" }
-function gitfp([string]$Arg1, [string]$Arg2) { git push -f "$Arg1" "$Arg2" }
-function gitr([string]$Arg1) { git reset "$Arg1" }
-function gitrh([string]$Arg1) { git reset --hard "$Arg1" }
+function gitf { git fetch @args }
+function gitp { git push @args }
+function gitfp { git push -f @args }
+function gitr { git reset @args }
+function gitrh { git reset --hard @args }
 
 # Clear terminal once profile is finished loading
 clear
