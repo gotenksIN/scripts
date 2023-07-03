8ad5a732e9f96e8f1910021df53f23f3a081195f
Omkar Chandorkar <gotenksIN@aospa.co>
2023-07-03 08:06:25 +0530

    ubuntu: setup: install telegram-desktop via flatpak
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -39,7 +39,9 @@ sudo rm packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/edge/ stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo nala update
-sudo nala install microsoft-edge code font-manager
+sudo nala install microsoft-edge code font-manager flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.telegram.desktop
 fi
 fi
 
