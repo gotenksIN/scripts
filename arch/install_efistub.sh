19208e86869cc07a264d97a153a04054dec03902
Omkar Chandorkar <gotenksIN@aospa.co>
2023-09-17 09:48:30 +0530

    arch: install_efistub: Warn users to run with root
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- arch/install_efistub.sh
+++ arch/install_efistub.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Run the script with root, sudo somehow breaks it
+
 read -e -p "Enter your boot disk device (e.g. /dev/sda): " -i "/dev/sda" disk
 read -e -p "Enter partition number for your boot partition (e.g. if your /boot is in /dev/sda1, enter 1): " -i "1" part
 read -e -p "Enter loader name (e.g. vmlinuz-linux): " -i "vmlinuz-linux" loader
@@ -11,19 +13,19 @@ read -e -p "Do you have an Intel or AMD CPU? (Y/n): " input
 if [[ "$input" =~ ^[Yy]$ ]]; then
         read -e -p "Enter 1 for Intel and 2 for AMD: " cpu
         if [[ "$cpu" =~ ^[1]$ ]]; then
-        sudo pacman -Sy intel-ucode
+        pacman -Sy intel-ucode
         microcode="\intel-ucode.img"
         printf -v largs "%s " \
-                "root=UUID=$(sudo findmnt -kno UUID /) rw" \
-                "rootfstype=$(sudo findmnt -kno FSTYPE /)" \
+                "root=UUID=$(findmnt -kno UUID /) rw" \
+                "rootfstype=$(findmnt -kno FSTYPE /)" \
                 "initrd"=${microcode} "initrd=${initrd}" \
                 quiet splash
         else
-        sudo pacman -Sy amd-ucode
+        pacman -Sy amd-ucode
         microcode="\amd-ucode.img"
         printf -v largs "%s " \
-                "root=UUID=$(sudo findmnt -kno UUID /) rw" \
-                "rootfstype=$(sudo findmnt -kno FSTYPE /)" \
+                "root=UUID=$(findmnt -kno UUID /) rw" \
+                "rootfstype=$(findmnt -kno FSTYPE /)" \
                 "initrd"=${microcode} "initrd=${initrd}" \
                 initcall_blacklist=acpi_cpufreq_init \
                 amd_pstate.shared_mem=1 \
@@ -31,9 +33,9 @@ if [[ "$input" =~ ^[Yy]$ ]]; then
         fi
 else
         printf -v largs "%s " \
-                "root=UUID=$(sudo findmnt -kno UUID /) rw" \
-                "rootfstype=$(sudo findmnt -kno FSTYPE /)" \
+                "root=UUID=$(findmnt -kno UUID /) rw" \
+                "rootfstype=$(findmnt -kno FSTYPE /)" \
                 "initrd=${initrd}" quiet splash
 fi
 
-sudo efibootmgr -c -d "${disk}" -p "${part}" -L "${label}" -l "${loader}" -u "${largs%* } ${extra}" --verbose
+efibootmgr -c -d "${disk}" -p "${part}" -L "${label}" -l "${loader}" -u "${largs%* } ${extra}" --verbose
