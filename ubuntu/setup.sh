392308162f5faba61bdaca87f4460787c64a0ec0
Omkar Chandorkar <gotenksIN@aospa.co>
2023-09-17 09:31:03 +0530

    ubuntu: setup: fix microsoft-edge installation
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -36,10 +36,10 @@ if [[ "$input" =~ ^[Yy]$ ]]; then
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo rm packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/edge/ stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/edge/ stable main" > /etc/apt/sources.list.d/microsoft-edge.list'
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo nala update
-sudo nala install microsoft-edge code font-manager flatpak
+sudo nala install microsoft-edge-stable code font-manager flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.telegram.desktop
 fi
