4136adfb36a55f2998e1d5b7687dc71debedf0b9
Omkar Chandorkar <gotenksIN@aospa.co>
2025-03-11 08:15:16 +0530

    common: wezterm: Add Ctrl+Click shortcut
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -14,6 +15,19 @@ config.font_size = 10.5
 config.initial_cols = 150
 config.initial_rows = 40
 config.window_background_opacity = 0.85
-
+config.mouse_bindings = {
+    -- Bind 'Up' event of CTRL-Click to open hyperlinks
+    {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'CTRL',
+      action = act.OpenLinkAtMouseCursor,
+    },
+    -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+    {
+      event = { Down = { streak = 1, button = 'Left' } },
+      mods = 'CTRL',
+      action = act.Nop,
+    },
+  }
 -- and finally, return the configuration to wezterm
 return config
