670beaa9f12c51c48c65cbb605c35c9c5a560879
Omkar Chandorkar <gotenksIN@aospa.co>
2026-03-10 13:55:52 +0530

    ubuntu: debloat: Update for 25.10
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- ubuntu/debloat_ubuntu.sh
+++ ubuntu/debloat_ubuntu.sh
@@ -3,13 +3,13 @@
 sudo snap remove firefox
 
 sudo apt autoremove --purge snapd gnome-software-plugin-snap apparmor rsyslog apport ^vim mobile-broadband-provider-info ^cloud \
-hexchat thunderbird transmission ^libreoffice gnote graphviz ^imagemagick celluloid rhythmbox redshift fonts-arundina \
+hexchat thunderbird transmission ^libreoffice gnote graphviz ^imagemagick celluloid rhythmbox redshift ^cups ^printer-driver print-manager \
 gnome-2048 gnome-calculator gnome-calendar gnome-chess gnome-mahjongg gnome-software-plugin-snap gnome-sudoku bolt ^libreoffice \
 language-pack-de-base language-pack-es-base popularity-contest language-pack-ru-base gnome-disk-utility gnome-font-viewer ^gnome-games \
 hunspell-de-at-frami hunspell-de-ch-frami hunspell-de-de-frami hunspell-en-au hunspell-en-ca hunspell-en-za hunspell-es hunspell-fr \
 hunspell-fr-classical hunspell-it hunspell-pt-br hunspell-pt-pt hunspell-ru language-pack-fr-base language-pack-pt-base unattended-upgrades \
-fonts-beng fonts-deva fonts-gujr fonts-indic fonts-knda fonts-mlym fonts-orya fonts-smc fonts-taml fonts-telu fonts-tibetan-machine fonts-thai-tlwg \
-fonts-kacst fonts-kacst-one fonts-khmeros-core fonts-lao fonts-lklug-sinhala fonts-sil-abyssinica fonts-sil-padauk fonts-arphic-ukai fonts-arphic-uming
+plasma-discover kate elisa haruna info kinfocenter kcalc kcharselect kdeconnect partitionmanager kmahjongg kmines krdc plasma-systemmonitor \
+kpat ksudoku ksystemlog neochat okular skanlite skanpage usb-creator-kde memtest86+
 
 sudo rm -rf /var/cache/snapd/ ~/snap
 
