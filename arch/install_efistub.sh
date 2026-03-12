6e021e36037a5ddbdfcf88941d22b3e55419ff2c
Omkar Chandorkar <gotenksIN@aospa.co>
2026-03-12 09:05:12 +0530

    arch: install_efistub: Escalate to root instead
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- arch/install_efistub.sh
+++ arch/install_efistub.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
+[ -n "${BASH_VERSION:-}" ] || exec bash "$0" "$@"
 
-# Run the script with root, sudo somehow breaks it
-
-if [[ ${EUID} -ne 0 ]]; then
-        echo "Run this script as the root user."
-        exit 1
-fi
-
-if [[ -n ${SUDO_USER:-} ]]; then
-        echo "Do not run this script with sudo. Switch to root first, then run it directly."
-        exit 1
+# Re-run with bash as root when invoked from a user shell.
+if (( EUID != 0 )); then
+        exec sudo --preserve-env=TERM bash "$0" "$@"
 fi
 
 read -e -p "Enter your boot disk device (e.g. /dev/sda): " -i "/dev/sda" disk
@@ -19,26 +13,27 @@ read -e -p "Enter initrd name (e.g. initramfs-linux.img): " -i "initramfs-linux.
 read -e -p "Enter label for this entry (Label is what would show up in UEFI entries): " -i "rendumOS (efistub)" label
 read -e -p "Enter any extra arguments you want to pass to kernel cmdline: " extra
 
+root_uuid=$(findmnt -kno UUID /)
+root_fstype=$(findmnt -kno FSTYPE /)
+initrd_args=("initrd=${initrd}")
+
 read -e -p "Do you have an Intel or AMD CPU? (Y/n): " input
 if [[ "$input" =~ ^[Yy]$ ]]; then
         read -e -p "Enter 1 for Intel and 2 for AMD: " cpu
         if [[ "$cpu" =~ ^[1]$ ]]; then
-        pacman -Sy intel-ucode
-        microcode="\intel-ucode.img"
+                pacman -Sy intel-ucode
+                microcode="\\intel-ucode.img"
         else
-        pacman -Sy amd-ucode
-        microcode="\amd-ucode.img"
+                pacman -Sy amd-ucode
+                microcode="\\amd-ucode.img"
         fi
-        printf -v largs "%s " \
-                "root=UUID=$(findmnt -kno UUID /) rw" \
-                "rootfstype=$(findmnt -kno FSTYPE /)" \
-                "initrd"=${microcode} "initrd=${initrd}" \
-                quiet splash
-else
-        printf -v largs "%s " \
-                "root=UUID=$(findmnt -kno UUID /) rw" \
-                "rootfstype=$(findmnt -kno FSTYPE /)" \
-                "initrd=${initrd}" quiet splash
+        initrd_args=("initrd=${microcode}" "${initrd_args[@]}")
 fi
+
+printf -v largs "%s " \
+        "root=UUID=${root_uuid} rw" \
+        "rootfstype=${root_fstype}" \
+        "${initrd_args[@]}" \
+        quiet splash
 
 efibootmgr -c -d "${disk}" -p "${part}" -L "${label}" -l "${loader}" -u "${largs%* } ${extra}" --verbose
