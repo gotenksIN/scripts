7538db8d2672634459585c0759f37e2f7743e77c
Omkar Chandorkar <gotenksIN@aospa.co>
2026-03-12 09:08:57 +0530

    RIP Cinnamon
    - Thanks tile-pusher
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- ubuntu/install_cinnamon.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo add-apt-repository ppa:aslatter/ppa
-sudo add-apt-repository ppa:ubuntucinnamonremix/unstable
-
-sudo apt install cinnamon lightdm slick-greeter arc-theme papirus-icon-theme dconf-editor alacritty
