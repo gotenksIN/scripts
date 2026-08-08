7538e31a3d019598846fb2225f1b51750468bb4b
Omkar Chandorkar <gotenksIN@aospa.co>
2026-08-08 10:06:43 +0530

    wezterm: align palette and styling with Konsole
    Match Konsole exact Catppuccin Frappe RGB palette, font, and 1.0 opacity.
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -17,16 +17,50 @@ local right_click_clipboard = wezterm.action_callback(function(window, pane)
   window:perform_action(act.PasteFrom 'Clipboard', pane)
 end)
 
--- This is where you actually apply your config choices
-config.color_scheme = 'Catppuccin Frappe'
+-- Exact RGB palette matching Konsole's catppuccin-frappe.colorscheme
+config.colors = {
+  foreground = '#c6d0f5',
+  background = '#303446',
+  cursor_bg = '#f2d5cf',
+  cursor_border = '#f2d5cf',
+  cursor_fg = '#303446',
+  selection_bg = '#626880',
+  selection_fg = '#c6d0f5',
+
+  ansi = {
+    '#737994', -- Black
+    '#e78284', -- Red
+    '#a6d189', -- Green
+    '#e5c890', -- Yellow
+    '#8caaee', -- Blue
+    '#ca9ee6', -- Magenta
+    '#99d1db', -- Cyan (Exact Konsole #99D1DB)
+    '#c6d0f5', -- White
+  },
+  brights = {
+    '#737994', -- Bright Black
+    '#e78284', -- Bright Red
+    '#a6d189', -- Bright Green
+    '#e5c890', -- Bright Yellow
+    '#8caaee', -- Bright Blue
+    '#ca9ee6', -- Bright Magenta
+    '#99d1db', -- Bright Cyan (#99D1DB)
+    '#c6d0f5', -- Bright White
+  },
+}
+
+config.bold_brightens_ansi_colors = "No"
+
 config.default_cursor_style = 'SteadyBar'
-config.enable_scroll_bar=true
+config.enable_scroll_bar = false
 config.enable_tab_bar = false
-config.font = wezterm.font 'MesloLGS Nerd Font'
+config.font = wezterm.font 'MesloLGL Nerd Font Mono'
 config.font_size = 10.5
 config.initial_cols = 150
 config.initial_rows = 40
-config.window_background_opacity = 0.85
+config.window_background_opacity = 1.0
+config.window_padding = { left = 4, right = 4, top = 4, bottom = 4 }
+
 config.keys = {
     -- Make terminal-side clipboard shortcuts explicit for TUIs like Codex.
     { key = 'C', mods = 'CTRL|SHIFT', action = act.CopyTo 'Clipboard' },
@@ -34,6 +68,7 @@ config.keys = {
     { key = 'Insert', mods = 'CTRL', action = act.CopyTo 'PrimarySelection' },
     { key = 'Insert', mods = 'SHIFT', action = act.PasteFrom 'PrimarySelection' },
 }
+
 config.mouse_bindings = {
     -- Bind 'Up' event of CTRL-Click to open hyperlinks
     {
@@ -59,5 +94,6 @@ config.mouse_bindings = {
       action = act.Nop,
     },
   }
--- and finally, return the configuration to wezterm
+
+-- Return the configuration to wezterm
 return config
